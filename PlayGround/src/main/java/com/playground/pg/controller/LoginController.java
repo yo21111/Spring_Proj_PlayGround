aec2d748b41087976c0af379bd5b8a2b177c205e
user <user@192.168.0.9>
2022-05-25 17:22:35 +0900

    Login Controller Modify 5/25

--- PlayGround/src/main/java/com/playground/pg/controller/LoginController.java
+++ PlayGround/src/main/java/com/playground/pg/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 	
 	@GetMapping("/Login")
-	public String Login() {
+	public String Login(String pn) {
 	    //메인페이지에서 로그인 버튼 클릭시 또는 아이디찾기 비밀번호 찾기 후클릭시 이동  
 		
 		return"로그인페이지";
@@ -20,16 +20,24 @@ public class LoginController {
 	
 	
 	@PostMapping("/isLogin")
-	public String isLogin(String id, String Pw)throws Exception {
+	public String isLogin(String id, String Pw,String pn)throws Exception {
 //	@오토와일드
 // 로그인관련 서비스 
 	 	
 	        
-	     boolean LoginCK="로그인서비스.로그인메서드(String id String Pw)";
+	     boolean LoginCK=true; //"로그인서비스.로그인메서드(String id String Pw)";
       //메서드를 실행한 값을 boolean 으로 받는다
+	     
 		 if(LoginCK) {
+			 
+			 if(pn!=null) {
+				 
+			return"원래 있던 페이지 이동";
+			 }
 			 return"메인페이지 이동";
 			 //값이 true일 경우 메인페이지 이동
+			
+			 
 		 }else {
 			 return"redirect:/Login/Login?msg=아이디 또는 비밀번호를 확인해주세요";
 			 //값이 false 일경우 로그인페이지로 메세지 출력
@@ -68,30 +76,38 @@ public class LoginController {
 	}
 	
 	
-	@PostMapping("/pw")
+	@PostMapping("/FindPw")
 	public String FindPw(Model model) {
 	    //비밀번호 찾기 버튼 클릭시
-		String PW = "로그인서비스.비밀번호찾기 메서드";
+		String SearchPw = "로그인서비스.비밀번호찾기 메서드";
 		//메서드가 실행되어 비밀번호값을 반환 받는다.
-      String Pw= PW.substring(0,PW.length()-5);
-      //PW를 잘라서 Pw대입
+      String Pw= SearchPw.substring(0,SearchPw.length()-5);
+      //SearchPw를 잘라서 Pw대입
       String star = "*****";
      
        Pw.concat(star);
        //Pw에 star더하기
-       model.addAttribute("PW", Pw);
+       model.addAttribute("Pw", Pw);
 		return"아이디/비밀번호 찾기 페이지";
 	}
      
 	
 
-	  @PostMapping("/changePW")
-	  public String changePW(String Cgpw) {  //Change pw
-	  // 비밀번호 변경 클릭시
-	     "로그인서비스.비밀번호 변경메서드(String Cgpw)";
+	@GetMapping("/ChangePw")
+	public String ChangPw(String CP) {//ChangePw
+		//비밀번호 바꾸기 버튼클릭시
+		//input hidden을 만들고 버튼 클릭시 Jquery 사용해서 값 주기 
+		return"아이디/비밀번호 찾기";
+	}
+	
+	
+	  @PostMapping("/NewPw")
+	  public String NewPw(String NP) {  //New Pw
+	     // 비밀번호 변경 클릭시
+	// "로그인서비스.비밀번호 변경메서드(String Cgpw)";
 		  //update sql을 사용하여 input에 있는 값으로 바꾼다.
 		  
-		
+		//JSTL사용해서 아이디/비밀번호찾기에서 하겠습니다 
 		  return"로그인페이지";
 	  }
 	  
