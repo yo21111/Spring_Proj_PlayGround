3ecedb9d1e0734ee862f484778fbb94eb75a8308
user <user@192.168.0.9>
2022-05-25 16:08:22 +0900

    LoginCotroller 5/25

--- PlayGround/src/main/java/com/playground/pg/controller/LoginController.java
+++ PlayGround/src/main/java/com/playground/pg/controller/LoginController.java
@@ -1,11 +1,8 @@
 package com.playground.pg.controller;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,60 +11,89 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/Login")
 public class LoginController {
 	
+	@GetMapping("/Login")
+	public String Login() {
+	    //메인페이지에서 로그인 버튼 클릭시 또는 아이디찾기 비밀번호 찾기 후클릭시 이동  
+		
+		return"로그인페이지";
+	}
+	
+	
 	@PostMapping("/isLogin")
-	public String isLogin(HttpServletRequest req, HttpServletResponse resp)throws Exception {
+	public String isLogin(String id, String Pw)throws Exception {
 //	@오토와일드
 // 로그인관련 서비스 
-	 
-    String user  = "로그인서비스.로그인메서드";	
-//서비스 속 메서드가 실행되면 값을 반환받는다 	
-//		
-		
-	//	if(user==admin){
-//  req.setAtrribute("admin","true");	
-	     
-	     return "메인페이지";
-	         //메인페이지 이동	
-		
-
-//  }else{
-	     // req.setAtrribute("isLogin","true");
-		
-		//return"메인페이지"; 
-
-
+	 	
+	        
+	     boolean LoginCK="로그인서비스.로그인메서드(String id String Pw)";
+      //메서드를 실행한 값을 boolean 으로 받는다
+		 if(LoginCK) {
+			 return"메인페이지 이동";
+			 //값이 true일 경우 메인페이지 이동
+		 }else {
+			 return"redirect:/Login/Login?msg=아이디 또는 비밀번호를 확인해주세요";
+			 //값이 false 일경우 로그인페이지로 메세지 출력
+		 }
+			 
 	}
-	@PostMapping("/id")
-	public String SearchID(String name, String phone, String emali) {
-		//아이디 찾을때 쓰는 값들 
-		String ID = "로그인서비스.아이디찾기메서드 ";
+	
+
+	@GetMapping("/Find")
+	public String Findpage() {
+		
+	//아이디 및 비밀 번호 찾기 클릭시 이동
+		return"아이디/비밀번호 찾기페이지";
+		
+	}
+	
+	
+	
+	@PostMapping("/Find_phone")
+	public String FindID_Phone(Model model) {
+		//아이디 찾기 버튼 클릭시  
+		       String id  ="로그인 서비스.아이디찾기 메서드(String name, String phone )";
 		//sql에서 select에 사용하여 받는 아이디
-		
-		//req.setAtrribute("id",ID);
+		  model.addAttribute("id", id);                              
 		return"아이디/비밀번호 찾기 페이지";
 	}
-	@GetMapping("/pw")
-	public String SearchPw(String id, String phone, String email) {
 	
-		String PW = "로그인서비스.비밀번호찾기 메서드";
-		//메서드 실행시 
-		//select sql을 사용하여 비밀번호를 가져온다
-	//	req.setAtrribute("pw",PW);
-		return"아이디/비밀번호 찾기 페이지";
-	}
 
+	@PostMapping("/Find_email")
+	public String FindID_email(Model model) {
+		//아이디 찾기 버튼 클릭시  
+	       String id  ="로그인 서비스.아이디찾기 메서드(String name, String email )";
+			//sql에서 select에 사용하여 받는 아이디
+			  model.addAttribute("id", id);                              
+			return"아이디/비밀번호 찾기 페이지";
+	}
 	
-	//비밀번호 변경은 마이페이지에서 하자고 퍼블리셔분이 얘기해주셧습니다
-	//어떻게들 생각하시는지 몰라서 일단 만들었습니다.
-	  @GetMapping("/changePW")
+	
+	@PostMapping("/pw")
+	public String FindPw(Model model) {
+	    //비밀번호 찾기 버튼 클릭시
+		String PW = "로그인서비스.비밀번호찾기 메서드";
+		//메서드가 실행되어 비밀번호값을 반환 받는다.
+      String Pw= PW.substring(0,PW.length()-5);
+      //PW를 잘라서 Pw대입
+      String star = "*****";
+     
+       Pw.concat(star);
+       //Pw에 star더하기
+       model.addAttribute("PW", Pw);
+		return"아이디/비밀번호 찾기 페이지";
+	}
+     
+	
+
+	  @PostMapping("/changePW")
 	  public String changePW(String Cgpw) {  //Change pw
-	
-	   //로그인서비스.비밀번호 변경메서드;
+	  // 비밀번호 변경 클릭시
+	     "로그인서비스.비밀번호 변경메서드(String Cgpw)";
 		  //update sql을 사용하여 input에 있는 값으로 바꾼다.
 		  
-		  //return"로그인페이지"  //만일 마이페이지가 아닌 아이디/비밀번호 찾기에서 할경우
-		  return"마이페이지";
+		
+		  return"로그인페이지";
 	  }
-	  //s
+	  
 	
 }
