8a932a1f4c08abc9979a7cd1558324cbd94d647e
Seojw <jeongwon4103@daum.net>
2022-05-27 15:40:17 +0900

    update join validation 0527

--- PlayGround/src/main/java/com/playground/pg/service/JoinServiceImpl.java
+++ PlayGround/src/main/java/com/playground/pg/service/JoinServiceImpl.java
@@ -67,6 +67,33 @@ public class JoinServiceImpl implements JoinService {
 		boolean result = true;
 		
 		// 유효성 검사 작성
+		// 유효성 검사 작성
+				//1. 각 값들이 null이 아닌지 확인 + 2. 각 값들에 대한 유효성 검사
+				String id = mDto.getId();
+				if (id == null || idCheck(id)) {
+					return false;
+				} 
+				
+				String[] nullChkArr = {mDto.getPw(), mDto.getName(), mDto.getPhone(), mDto.getEmail()};
+				for (int i = 0; i < nullChkArr.length; i++) {
+					if (nullChkArr[i] == null) {
+						return false;
+					}
+				}
+				
+				String birth = mDto.getBirth();
+				String[] birthSep = birth.split("-");
+				if (birthSep[0].length() > 4) {
+					return false;
+				}
+				int month = Integer.parseInt(birthSep[1]);
+				int day = Integer.parseInt(birthSep[2]);
+				if (1 > month && month > 12) {
+					return false;
+				}
+				if (1 > day && day > 31) {
+					return false;
+				}
 		
 		return result;
 	}
